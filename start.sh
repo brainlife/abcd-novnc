fb3636ac43db686a467b9dcb7cb42adf532b9d49
Soichi Hayashi <hayashis@iu.edu>
2017-04-05 03:24:59 +0000

    added a bit of debug log

--- start.sh
+++ start.sh
@@ -6,10 +6,8 @@ if [ -z $INST_DIR ]; then export INST_DIR=`pwd`; fi
 if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
 (cd $SERVICE_DIR && npm install)
+echo "current directory"
+pwd
+echo "running $node $SERVICE_DIR/setup"
 node $SERVICE_DIR/setup
-
-#echo "starting docker container"
-#id=$(docker run -dP -v `pwd`/../$input_task_id:/input:ro soichih/vncserver-fsl)
-#echo "started container $id"
-
 
