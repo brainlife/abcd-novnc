f98dde9b188e319e861396ca0467b0c5e972dc9a
Soichi Hayashi <hayashis@iu.edu>
2017-04-15 22:21:39 +0000

    passing X11VNC_PASSWORD via env

--- setup.js
+++ setup.js
@@ -37,70 +37,82 @@ default:
     console.error("unknown container type", config.type);
 }
 
-console.log('docker', ['run', '-dP', '-v', abs_src_path+':/input:ro', container_name]); 
-const cont = spawn('docker', ['run', '-dP', '-v', abs_src_path+':/input:ro', container_name]); 
-var cont_id = "";
-cont.stdout.on('data', (data)=>{
-    cont_id+=data.toString().trim();
+const pull = spawn('docker', ['pull', container_name]); 
+pull.stdout.on('data', (data)=>{
+    console.log(data.toString());
 });
-cont.stderr.on('data', (data)=>{
+pull.stderr.on('data', (data)=>{
     console.error(data.toString());
 });
-cont.on('close', (code)=>{
-    if(code != 0) throw new Error("failed to start container. code:"+ code);
-    console.log("container started",cont_id);
+pull.on('close', (code)=>{
+    if(code != 0) throw new Error("failed to pull container. code:"+ code);
 
-    fs.writeFileSync("cont.id", cont_id);
-
-    //find host:port that container listens to
-    const getp = spawn('docker', ['port', cont_id]);
-    var rep = "";
-    getp.stdout.on('data', (data)=>{
-        rep += data.toString().trim();
+    console.log('docker', ['run', '-dP', '-v', abs_src_path+':/input:ro', container_name]); 
+    const cont = spawn('docker', ['run', '-dP', '-e', 'X11VNC_PASSWORD='+password, '-v', abs_src_path+':/input:ro', container_name]); 
+    var cont_id = "";
+    cont.stdout.on('data', (data)=>{
+        cont_id+=data.toString().trim();
     });
-    getp.stderr.on('data', (data)=>{
+    cont.stderr.on('data', (data)=>{
         console.error(data.toString());
     });
-    getp.on('close', (code)=>{
-        if(code != 0) throw new Error("failed to get docker host/port");
-        var hostport = rep.split(" ")[2];
-        console.log("container listening on ", hostport);
-        
-        //set password for vncserver
-        require('crypto').randomBytes(4, function(err, buffer) {
-            const token = buffer.toString('hex');
-            //console.log('docker', ['exec', cont_id, 'bash', '-c', "echo -e '"+token+"\n"+token+"\nn' | vncpasswd"]); 
-            const setpass = spawn('docker', ['exec', cont_id, 'bash', '-c', "echo -e '"+token+"\n"+token+"\nn' | vncpasswd"]); 
-            setpass.stderr.on('data', (data)=>{
-                console.error(data.toString());
-            });
-            setpass.on('close', (code)=>{
-                if(code != 0) throw new Error("failed to set password for vncserver");
+    cont.on('close', (code)=>{
+        if(code != 0) throw new Error("failed to start container. code:"+ code);
+        console.log("container started",cont_id);
 
-                //find open port to use
-                tcpportused.findFree(11000, 12000, '0.0.0.0')
-                .then(function(port) {
-                    //start noVNC
-                    const novnc_out = fs.openSync('./novnc.log', 'a');
-                    const novnc_err = fs.openSync('./novnc.log', 'a');
-                    console.log('/usr/local/noVNC/utils/launch.sh', '--listen', port, '--vnc', hostport);
-                    const novnc = spawn('/usr/local/noVNC/utils/launch.sh', ['--listen', port, '--vnc', hostport], {
-                        detached: true, stdio: ['ignore', novnc_out, novnc_err]
+        fs.writeFileSync("cont.id", cont_id);
+
+        //find host:port that container listens to
+        const getp = spawn('docker', ['port', cont_id]);
+        var rep = "";
+        getp.stdout.on('data', (data)=>{
+            rep += data.toString().trim();
+        });
+        getp.stderr.on('data', (data)=>{
+            console.error(data.toString());
+        });
+        getp.on('close', (code)=>{
+            if(code != 0) throw new Error("failed to get docker host/port");
+            var hostport = rep.split(" ")[2];
+            console.log("container listening on ", hostport);
+            
+            //set password for vncserver
+            require('crypto').randomBytes(4, function(err, buffer) {
+                const token = buffer.toString('hex');
+                const setpass = spawn('docker', ['exec', cont_id, 'bash', '-c', "echo -e '"+token+"\n"+token+"\nn' | vncpasswd"]); 
+                setpass.stderr.on('data', (data)=>{
+                    console.error(data.toString());
+                });
+                setpass.on('close', (code)=>{
+                    if(code != 0) throw new Error("failed to set password for vncserver");
+
+                    //find open port to use
+                    tcpportused.findFree(11000, 12000, '0.0.0.0')
+                    .then(function(port) {
+                        //start noVNC
+                        const novnc_out = fs.openSync('./novnc.log', 'a');
+                        const novnc_err = fs.openSync('./novnc.log', 'a');
+                        console.log('/usr/local/noVNC/utils/launch.sh', '--listen', port, '--vnc', hostport);
+                        const novnc = spawn('/usr/local/noVNC/utils/launch.sh', ['--listen', port, '--vnc', hostport], {
+                            detached: true, stdio: ['ignore', novnc_out, novnc_err]
+                        });
+                        novnc.unref();
+
+                        console.log("started novnc", novnc.pid);
+                        fs.writeFileSync("novnc.pid", novnc.pid);
+
+                        var url = "http://"+os.hostname()+":"+port+"/vnc_auto.html?password="+token;
+                        fs.writeFileSync("url.txt", url);
+                        console.log("all done", url);
+                    }, function(err) {
+                        console.log("throwing now");
+                        throw err;
                     });
-                    novnc.unref();
-
-                    console.log("started novnc", novnc.pid);
-                    fs.writeFileSync("novnc.pid", novnc.pid);
-
-                    var url = "http://"+os.hostname()+":"+port+"/vnc_auto.html?password="+token;
-                    fs.writeFileSync("url.txt", url);
-                    console.log("all done", url);
-                }, function(err) {
-                    console.log("throwing now");
-                    throw err;
                 });
             });
         });
-    });
 
+    });
 });
+
+
