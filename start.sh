2a853a564ff885a931395c5fc53b2c815b6584ed
Soichi Hayashi <hayashis@iu.edu>
2017-12-20 15:11:22 -0500

    added rm url.txt to prevent premature novnc startup

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@
 if [ -z $INST_DIR ]; then export INST_DIR=`pwd`; fi
 if [ -z $SERVICE_DIR ]; then export SERVICE_DIR=`pwd`; fi
 
+rm url.txt #prevent premature novnc startup in case rerun
+
 (cd $SERVICE_DIR && npm install)
 echo "current directory"
 pwd
