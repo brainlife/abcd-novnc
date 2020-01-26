d4a7c76b3e5d9d965cbfe50c971bbfa93b9ccfa6
Soichi Hayashi <hayashis@iu.edu>
2020-01-26 18:04:05 -0500

    /usr/lib/x86_64-linux-gnu/tls went missing

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ cp -av /usr/lib/x86_64-linux-gnu/libEGL* lib
 cp -av /usr/lib/x86_64-linux-gnu/libnvidia* lib
 cp -av /usr/lib/x86_64-linux-gnu/libnvoptix* lib
 cp -r -av /usr/lib/x86_64-linux-gnu/vdpau lib
-cp -r -av /usr/lib/x86_64-linux-gnu/tls lib
+#cp -r -av /usr/lib/x86_64-linux-gnu/tls lib
 #cp -r -av /usr/lib/x86_64-linux-gnu/nvidia* lib
 
 #somehow I can't install this globally
